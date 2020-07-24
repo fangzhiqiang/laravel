9be194c911631c492446782c7c56c2f00856bd47
joker <1330550500@qq.com>
2020-07-24 16:11:12 +0800

    test

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::get('/', function () {
 });
 
 Route::post('register','AuthController@register');
-Route::post('login','AuthController@login');
+Route::post('api/login','AuthController@login');
 Route::get('test1','AuthController@test1');
 Route::prefix('admin')->middleware(['auth:api'])->group(function(){
     Route::get('logout','AuthController@logout');
