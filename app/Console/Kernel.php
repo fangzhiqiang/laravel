6ceaec30be43d3e7496208ab60d13e50b4446878
joker <1330550500@qq.com>
2020-07-30 09:44:44 +0800

    test

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,6 +26,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
+        echo 111;
         // $schedule->command('inspire')->hourly();
         $schedule->call(function(){
             User::destroy(11);
